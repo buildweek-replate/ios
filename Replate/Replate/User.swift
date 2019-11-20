d6a7e37ca1e5b50c5915e82125db97a33d18fb63
Rwoltx <richardx1016@gmail.com>
2019-11-19 23:45:37 -0500

    Sign up iOS UI complete

---
+++ Replate/Replate/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  Replate
+//
+//  Created by Rick Wolter on 11/19/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    
+    
+    
+    
+    
+}
